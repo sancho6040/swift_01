9a5bf07c9f21580123d9b393ee1d56d9ed0ec223
sancho6040 <56619610+sancho6040@users.noreply.github.com>
2019-10-31 19:43:26 -0500

    Update reto_parquiadero.swift

--- reto_parquiadero.swift
+++ reto_parquiadero.swift
@@ -25,12 +25,29 @@ arl2 += [userStruc]
 
 //prueba con tuplas
 
-var user:(nombre:String, marca:String, placa: String, id:String) 
+var pCarros = 0
+var pMotos = 0
+var pBicicletas = 0
+
+func randomEntrada() -> Int{
+  let number = Int.random(in: 4..<12)
+  return number
+}
+
+func randomSalida() -> Int{
+  let number = Int.random(in: 12...22)
+  return number
+}
+
+var user:(nombre:String, marca:String, placa: String, id:String, hEnt:Int, hSal:Int) 
 
 user = (nombre: "jhon",
         marca: "audi",
         placa: "asd459",
-        id: "123")
+        id: "123",
+        hEnt: randomEntrada(),
+        hSal: randomSalida()
+        )
 
 var arl = [user]
 
@@ -65,7 +82,10 @@ func agregarUsuario(){
   user = (nombre: agrNombre!,
         marca: agrMarca!,
         placa: agrPlaca!,
-        id: agrID!)
+        id: agrID!,
+        hEnt: randomEntrada(),
+        hSal: randomSalida()
+        )
 
   arl += [user]
 }
@@ -74,40 +94,66 @@ func entrarInvitado(){}
 
 
 var opcMenu:String?
-  //var opcMenu2:String?
+var opcVehiculo:String? = nil
 
   while(opcMenu != "3"){
     print("esrciba 3 para salir")
 
-    print("es un usuario registrado? (si/no)")
+    print("desea ingresar al parquiadero? (si/no)")
     opcMenu = readLine()
+    if(opcMenu == "si"){
+      
+      while(opcVehiculo == nil){
+        print("que vehiculo va a ingresar?(carro, moto, bicicleta)")
+        opcVehiculo = readLine()
 
-    if (opcMenu == "si"){
-      //buscar usuario
-      busquedaUsuario()
+        if(opcVehiculo == "carro"){
 
-    }else if (opcMenu == "no"){
-      print("desea registrarse como usuraio? (si/no)")
+        }else if(opcVehiculo == "moto"){
+
+        }else if(opcVehiculo == "bicicleta"){
+
+        }else{
+          print("opcion incorrecta")
+          opcVehiculo = nil
+        }
+      }
+
+      print("es un usuario registrado? (si/no)")
       opcMenu = readLine()
+
+      if (opcMenu == "si"){
+        //buscar usuario
+        busquedaUsuario()
+
+      }else if (opcMenu == "no"){
+        print("desea registrarse como usuraio? (si/no)")
+        opcMenu = readLine()
 
         if (opcMenu == "si"){
           //añadir usuario
           agregarUsuario()
+
         }else if(opcMenu == "no"){
           //entrar como invitado
           entrarInvitado()
-        }
 
-    }else if (opcMenu == "3"){
+        }
+      }else if (opcMenu == "3"){
+        print("salir")
+
+      }else{ print("opcion incorrecta")}
+
+    }else if(opcMenu == "3"){
       print("salir")
 
+    }else if(opcMenu == "no"){
+      print("desea retirar un vehiculo del parquiadero? (si/no)")
+
     }else{ print("opcion incorrecta")}
+    
 
   }
-
-
-
-
 
 
 print("____")
