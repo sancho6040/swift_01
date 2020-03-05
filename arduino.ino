6845268996c5764bfcb8d4537908ae3e5da6c7b3
sancho6040 <56619610+sancho6040@users.noreply.github.com>
2020-03-04 23:46:06 -0500

    eventos + LOW
    código con eventos del teclado y  condicionales en LOW
    -se eliminaron las variables temporales

--- arduino.ino
+++ arduino.ino
@@ -1,59 +1,112 @@
+//import simalacion del teclado
+#include <Keyboard.h>
 
-int up = 2;
-int down = 3;
-int r = 4;
-int l = 5;
-int start = 6;
-int buttonA = 7;
-int buttonB = 8;
+//boton start
+int start = 0;
 
-int tempU;
-int tempD;
-int tempR;
-int tempL;
-int tempS;
-int tempA;
-int tempB;
+//joystick 1
+int up1 = 1;
+int down1 = 2;
+int R1 = 3;
+int L1 = 4;
+int buttonA1 = 5;
+int buttonB1 = 6;
+
+//joystick 2
+int up2 = 7;
+int down2 = 8;
+int R2 = 9;
+int L2 = 10;
+int buttonA2 = 11;
+int buttonB2 = 12;
+
 
 void setup() {
-  //cofiguracion inicial
-  //pinMode(led, OUTPUT);
+  
+  //inicia la comunicacion serial
   Serial.begin(9600);
   
-  pinMode(up, INPUT);
-  pinMode(down, INPUT);
-  pinMode(r, INPUT);
-  pinMode(l, INPUT);
+  // definicion de pins Joystick 1
+  pinMode(up1, INPUT);
+  pinMode(down1, INPUT);
+  pinMode(R1, INPUT);
+  pinMode(L1, INPUT);
+  pinMode(buttonA1, INPUT);
+  pinMode(buttonB1, INPUT);
+  
+  // definicion de pins Joystick 2
+  pinMode(up2, INPUT);
+  pinMode(down2, INPUT);
+  pinMode(R2, INPUT);
+  pinMode(L2, INPUT);
+  pinMode(buttonA2, INPUT);
+  pinMode(buttonB2, INPUT);
+  
+  // definicion de pin start
   pinMode(start, INPUT);
-  pinMode(buttonA, INPUT);
-  pinMode(buttonB, INPUT);
 }
 
 void loop() {
-  //bucle
-  tempU = digitalRead(up);
-  tempD = digitalRead(down);
-  tempR = digitalRead(r);
-  tempL = digitalRead(l);
-  tempS = digitalRead(start);
-  tempA = digitalRead(buttonA);
-  tempB = digitalRead(buttonB);
   
-  if(tempU == HIGH){
-    Serial.println("arriba");
-  }if(tempD == HIGH){
-    Serial.println("abajo");
-  }if(tempR == HIGH){
-    Serial.println("derecha");    
-  }if(tempL == HIGH){
-    Serial.println("izquierda");
-  }if(tempS == HIGH){
+  //condicional de start
+  if(digitalRead(start) == LOW){
     Serial.println("start");
-  }if(tempA == HIGH){
-    Serial.println("boton A");
-  }if(tempB == HIGH){
-    Serial.println("boton B");
+    Keyboard.write(13);// start = ENTER
   }
+  
+  //condicionales de joystick 1
+  if(digitalRead(up1) == LOW){
+    Serial.println("arriba");
+     Keyboard.write(87); // up1 = w
+  }
+  if(digitalRead(down1) == LOW){
+    Serial.println("abajo");
+    Keyboard.write(83); //down1 = s
+  }
+  if(digitalRead(R1) == LOW){
+    Serial.println("derecha");    
+    Keyboard.write(68); // derecha_1 = d
+  }
+  if(digitalRead(L1) == LOW){
+    Serial.println("izquierda");
+    Keyboard.write(65); // izquierda_1 = a
+  }
+  if(digitalRead(buttonA1) == LOW){
+    Serial.println("boton A");
+    Keyboard.write(70); //boton A1 = f
+  }
+  if(digitalRead(buttonB1) == LOW){
+    Serial.println("boton B");
+    Keyboard.write(71); //boton b1 = g
+  }
+  
+  
+  //condicionales de joystick 2
+  if(digitalRead(up2) == LOW){
+    Serial.println("arriba");
+    Keyboard.write(38); // up2 = flecha arriba
+  }
+  if(digitalRead(down2) == LOW){
+    Serial.println("abajo");
+    Keyboard.write(40); // down2 = flecha abajo
+  }
+  if(digitalRead(R2) == LOW){
+    Serial.println("derecha");    
+    Keyboard.write(39); // D2 = flecha derecha
+  }
+  if(digitalRead(L2) == LOW){
+    Serial.println("izquierda");
+    Keyboard.write(37); // L2 = flecha izquierda
+  }
+  if(digitalRead(buttonA2) == LOW){
+    Serial.println("boton A");
+    Keyboard.write(74); // boton A2 = j
+  }
+  if(digitalRead(buttonB2) == LOW){
+    Serial.println("boton B");
+    Keyboard.write(75); // boton B2 = k
+  }
+  
 
 
 }
